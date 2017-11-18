0edf9b05249cf82c51ae63a402230f3388bdaea7
skarafaz <skarafaz@gmail.com>
2017-11-18 19:00:02 +0100

    dev: sql

--- src/main/sql/1.0.3.sql
+++ src/main/sql/1.0.3.sql
@@ -1,2 +1,2 @@
 alter table incoming_file rename column downloaded to feed;
-alter table incoming_file alter column feed default set true;
+alter table incoming_file alter column feed set default true;
